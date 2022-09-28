e42950e7148f8486ff004f67d8cf44b289e3d1a3
HeyMita <matiasadcr@gmail.com>
2022-09-28 22:40:03 +0100

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+//import { LoginComponent } from './components/login/login.component';
 import { TodoComponent } from './components/todo/todo.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: LoginComponent
+    component: TodoComponent
   }
 ];
 
